7b34fbc0b3f742852e9b0a4808b39dda7b333af1
gull2365 <jych2365@gmail.com>
2024-01-30 23:20:32 +0900

    feat: Add hero letter animations

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,5 +1,10 @@
-import { Content } from "@prismicio/client";
+"use client";
+
+import { useEffect, useRef } from "react";
+
+import { Content, KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import gsap from "gsap";
 
 /**
  * Props for `Hero`.
@@ -10,14 +15,86 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
+  const componets = useRef(null);
+
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      const timeline = gsap.timeline();
+
+      timeline.fromTo(
+        ".name-animation",
+        {
+          x: -100,
+          opacity: 0,
+          roatate: -10,
+        },
+        {
+          x: 0,
+          opacity: 1,
+          rotate: 0,
+          ease: "elastic.out(1,0.3)",
+          duration: 1,
+          transformOrigin: "left top",
+          stagger: {
+            each: 0.08,
+            from: "random",
+          },
+        }
+      );
+
+      timeline.fromTo(
+        ".job-title",
+        { y: 20, opacity: 0 },
+        {
+          opacity: 1,
+          y: 0,
+          scale: 1,
+          duration: 1,
+          ease: "elastic.out(1,0.3)",
+        }
+      );
+    }, componets);
+    return () => ctx.revert();
+  }, []);
+
+  const rederLetters = (name: KeyTextField, key: string) => {
+    if (!name) return;
+    return name.split("").map((letter, index) => (
+      <span
+        key={index}
+        className={`name-animation name-animation-${key} inline-block opacity-0`}
+      >
+        {letter}
+      </span>
+    ));
+  };
+
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      ref={componets}
     >
-      {slice.primary.first_name}
-      {slice.primary.last_name}
-      {slice.primary.tag_line}
+      <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
+        <div className="col-start-1 md:row-start-1">
+          <h1
+            className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter"
+            aria-label={
+              slice.primary.first_name + " " + slice.primary.last_name
+            }
+          >
+            <span className="block text-slate-300">
+              {rederLetters(slice.primary.first_name, "first")}
+            </span>
+            <span className="-mt-[.2em] block text-slate-500">
+              {rederLetters(slice.primary.last_name, "last")}
+            </span>
+          </h1>
+          <span className="job-title block bg-gradient-to-tr from-blue-500 via-green-300 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-tighter-[.2em] text-transparent opacity-0 md:text-4xl">
+            {slice.primary.tag_line}
+          </span>
+        </div>
+      </div>
     </section>
   );
 };
