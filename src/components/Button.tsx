b2ac248f877ee3e457b6c1a785f069031bbd9a2a
gull2365 <jych2365@gmail.com>
2024-02-02 17:40:49 +0900

    caf: 일단 타입 지정만 먼저

---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import { KeyTextField, LinkField } from "@prismicio/client";
+
+type ButtonProps = {
+  linkField: LinkField;
+  label: KeyTextField;
+  showIcon?: boolean;
+  className?: string;
+};
+
