0ac3b74d2ead6a7e767a03fe8853ec76820a173e
gull2365 <jych2365@gmail.com>
2024-02-02 17:41:08 +0900

    err: type 에러 뜯어보기

--- src/slices/Biography/index.tsx
+++ src/slices/Biography/index.tsx
@@ -1,5 +1,7 @@
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import Bounded from "@/components/Bounded";
+import Heading from "@/components/Heading";
+import { Content, RichTextField } from "@prismicio/client";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
 /**
  * Props for `Biography`.
@@ -11,12 +13,19 @@ export type BiographyProps = SliceComponentProps<Content.BiographySlice>;
  */
 const Biography = ({ slice }: BiographyProps): JSX.Element => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for biography (variation: {slice.variation}) Slices
-    </section>
+      <div className="grid gap-x-8 gap-y-6 md:grid-cols-[2fr,1fr]">
+        <Heading as="h2" size="xl" className="col-start-1">
+          {slice.primary.heading}
+        </Heading>
+        <div className="prose prose-xl prose-slate prose-invert col-start-1">
+          {/* <PrismicRichText field={slice.primary.description} /> */}
+        </div>
+      </div>
+    </Bounded>
   );
 };
 
