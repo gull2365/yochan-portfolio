292472cba42e9551388b18bde3a2592ddfab629a
gull2365 <jych2365@gmail.com>
2024-02-18 14:53:32 +0900

    fix: modify settings data

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -66,15 +66,6 @@ export default async function Footer() {
               <FaGithub />
             </PrismicNextLink>
           )}
-          {isFilled.link(settings.data.email_link) && (
-            <PrismicNextLink
-              field={settings.data.email_link}
-              className="p-2 text-2xl text-slate-300 transition-all duration-150 hover:scale-125 hover:text-yellow-400"
-              aria-label={settings.data.name + " on Email"}
-            >
-              <FaTwitter />
-            </PrismicNextLink>
-          )}
           {isFilled.link(settings.data.linkedin_link) && (
             <PrismicNextLink
               field={settings.data.linkedin_link}
